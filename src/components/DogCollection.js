b729a8c8ac620b1bf464f400123b9e0c4316dd43
iankuria668 <iankuria668@gmail.com>
2024-05-07 12:00:52 +0300

    Event listener to adopt a dog included

--- src/components/DogCollection.js
+++ src/components/DogCollection.js
@@ -1,20 +1,30 @@
 import {useState, useEffect} from "react";
-import
+import YourAdoptedDogs from "./YourAdoptedDogs";
 function DogCollection() {
+    // Declacing states
     const [dogs, setDogs] = useState([]);
     const [adopteddogs, setAdoptedDogs] = useState([]);
 
+    // Fetching data from the API
     useEffect(() => {
         fetch('https://api.thedogapi.com/v1/breeds')
             .then(res => res.json())
             .then(data => {
                 setDogs(data);
+                // Troubleshooting
                 console.log("Successfully fetched data");
             })
+            // Error handling
             .catch(error =>{
                 console.log("Error fetching data")
             });
     }, []);
+
+    //Event Listener for the Adopt a dog button
+    const HandleAdopt = (event) => {
+        event.preventDefault();
+        setAdoptedDogs([...adopteddogs, event.target.id]);
+    }
 
     return (
         <div>
@@ -22,12 +32,14 @@ function DogCollection() {
             {dogs ? (
                 <div className="dog-collection">
                     {dogs.map((dog) => (
+                        // Returning a single division for each dog
                         <div className="dog-card" key={dog.id}>
                             <img src={dog.image.url} alt={dog.name} />
                             <h3>{dog.name}</h3>
                             <p>{dog.temperament}</p>
                             <button
-                            onClick={ }
+                            onClick={HandleAdopt}
+                            id={dog.id}
                             >Adopt</button>
                         <button>View Details</button>
                         </div>
