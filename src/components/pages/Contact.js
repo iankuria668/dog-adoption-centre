692282281ba0e767aab5bd2be792522571adadbd
+ <+>
2024-05-08 00:27:28 +0300

    Added some style

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,9 +11,9 @@ function Contact() {
         </header>
         <main>
         <div id="contact" className="contact">
-        <p><strong>Call us at this number: </strong> <em>071-234-567-890</em></p>
+        <p><strong><em>Call us at this number:</em> </strong> <em>071-234-567-890</em></p>
         <p><strong className="">If you are impresed with this group's work support us via Mpesa on: </strong> <em>071-283-7843 </em></p>
-        <p> <a href="mailto:billjoseph@gmail.com" className="link">Click here to Send us an email</a></p>
+        <p> <a href="mailto:billjoseph@gmail.com" className="link"><em>Click here to Send us an email</em></a></p>
         </div>
         </main>
         <footer>
