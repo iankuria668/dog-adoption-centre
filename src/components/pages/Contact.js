6a9ee3c08956d54101b9b0bba11085f7a435e100
joel langat <langatjoel42@gmail.com>
2024-05-08 08:10:30 +0300

    made some changes on pictures

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,9 @@
+// Contact.js
 import React from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
+import ContactImage1 from "./DogHome/BARK Post_ Dog News, Guides, Reviews, & Cool Stuff from BARK.jpeg"; // Import the first contact image
+import ContactImage2 from "./DogHome/These Lovely Dogs ‘Pack Walk’ And Pose For Pictures Together Every Day.jpeg"; // Import the second contact image
 
 function Contact() {
   return (
@@ -9,19 +12,24 @@ function Contact() {
         <NavBar />
       </header>
       <main className="container mx-auto p-8">
-        <div id="contact" className="contact">
-          <p>
-            <strong>Call us at this number:</strong> <em>071-234-567-890</em>
-          </p>
-          <p>
-            <strong>If you are impressed with this group's work support us via Mpesa on:</strong>{" "}
-            <em>071-283-7843</em>
-          </p>
-          <p>
-            <a href="mailto:billjoseph@gmail.com" className="text-blue-400">
-              Click here to Send us an email
-            </a>
-          </p>
+        <h1 className="text-4xl font-bold mb-8">Contact Us</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* First column */}
+          <div className="flex flex-col items-center">
+            <img src={ContactImage1} alt="Contact 1" className="w-100 h-100 mb-4" /> {/* First contact image */}
+           
+            
+          </div>
+          {/* Second column */}
+          <div className="flex flex-col items-center">
+            <img src={ContactImage2} alt="Contact 2" className="w-100 h-100 mb-4" /> {/* Second contact image */}
+          </div>
+          <div className="flex flex-col items-center">
+          <p>If you have any questions or inquiries, feel free to contact us using the information below:</p>
+          <p><strong>Call us:</strong> 071-234-567-890</p>
+            <p><strong>Support us via Mpesa:</strong> 071-283-7843</p>
+            <p><a href="mailto:billjoseph@gmail.com" className="text-green-400 hover:text-green-300">Click here to send us an email</a></p>
+          </div>
         </div>
       </main>
       <footer>
