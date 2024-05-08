b50b8de203101e4ee5d2c56baeccdce74ab34e4c
Bill Mageni <+>
2024-05-08 10:03:30 +0300

    Added some text in About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -46,32 +46,35 @@ function About() {
   };
 
   return (
-  <div className="">
+  <div className="bg-gray-950	background-color: rgb(3 7 18);">
     <header>
       <NavBar />
       </header>
-      <div className="about-section bg-black text-white">
+      <div className="about-section bg-black text-white container mx-auto p-9">
         <h1 className=" text-center text-9xl	font-size: 8rem; line-height: 1; text-gray-400	--tw-text-opacity: 1; color: rgba(156, 163, 175, var(--tw-text-opacity));">About Us </h1>
         <br></br>
         <br></br>
         <br></br>
-        <p className="text-4xl	font-size: 2.25rem;line-height: 2.5rem; ">General Mission:</p> <p className="text-align: center; text-xl	font-size: 1.25rem; line-height: 1.75rem; " >
+        <p className="text-center	text-align: center; text-4xl 	font-size: 2.25rem;line-height: 2.5rem; ">General Mission:</p> <p className="text-align: center; text-xl	font-size: 1.25rem; line-height: 1.75rem; text-justify	text-align: justify; " >
           At The Royal Pets, our mission is to rescue abandoned, neglected, and surrendered dogs, providing them with the care, training, and love they deserve. We strive to match each dog with its perfect forever home, ensuring a lifetime of happiness and companionship. We also promote responsible pet ownership and animal welfare through education and community outreach.</p>
           <br></br>
           <br></br>
           <br></br>
-          <p className="text-4xl	font-size: 2.25rem;line-height: 2.5rem;">Specific Breed Focus:</p><p className="text-center	text-align: center; text-xl	font-size: 1.25rem; line-height: 1.75rem;" >
+          <p className=" text-center	text-align: center; text-4xl	font-size: 2.25rem;line-height: 2.5rem;">Specific Breed Focus:</p><p className="text-center	text-align: center; text-xl	font-size: 1.25rem; line-height: 1.75rem; text-justify	text-align: justify; " >
           The Royal Pets is dedicated to rescuing and rehoming Labrador Retrievers. We are passionate about finding loving families for these wonderful dogs and ensuring they thrive in their forever homes. Through responsible adoption practices and education, we advocate for the well-being of all Labradors in need.</p>
           <br></br>
           <br></br>
           <br></br>
-          <p className="text-4xl	font-size: 2.25rem;line-height: 2.5rem;">Community Focus:</p><p className="text-center	text-align: center; text-xl	font-size: 1.25rem; line-height: 1.75rem;" >
+          <p className="text-center	text-align: center; text-4xl	font-size: 2.25rem;line-height: 2.5rem;">Community Focus:</p><p className="text-center	text-align: center; text-xl	font-size: 1.25rem; line-height: 1.75rem; text-justify	text-align: justify; " >
             The Royal Pets believes that every dog deserves a second chance. We partner with shelters and rescues to find homes for homeless dogs. We also offer low-cost veterinary services, spay/neuter programs, and training resources for our community, promoting responsible pet ownership and a supportive environment for all dogs and their families.</p>
           <br></br>
           <br></br>
           <br></br>
           </div>
-          <div className="container mx-auto p-8 ">
+          <h1 className="text-transparent	color: transparent;"> rvcef </h1>
+          <div className="container mx-auto p-8 bg-gray-900	background-color: rgb(17 24 39); ">
+            <h1 className="text-center text-6xl	font-size: 8rem; line-height: 1; text-gray-400	--tw-text-opacity: 1; color: rgba(156, 163, 175, var(--tw-text-opacity))"> Contact Us </h1>
+            <br></br>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
              {teamMembers.map((member, index) => (
              <div key={index} className="bg-gray-800 p-6 rounded-md shadow-md flex flex-col items-center">
