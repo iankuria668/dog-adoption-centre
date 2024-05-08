12d871440ccc43d61d81b514c54ecd0ae73f64ec
Bill Mageni <+>
2024-05-08 10:16:26 +0300

    Added some style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,7 @@ function Footer() {
     <footer className="bg-gray-900 text-white p-4">
       <div className="container mx-auto">
         <p className="text-sm	font-size: 0.875rem; /* 14px */ line-height: 1.25rem; /* 20px */"> 
-          <strong>Author:</strong>  <em>Group 3</em>
-          <h1 className="text-transparent	color: transparent;"> rvcef </h1>
-          <strong>Copyright:</strong> <em> Group3 2024 </em>
+          <strong>Author:</strong>  <em>Group 3</em> <h1 className="text-transparent	color: transparent;"> rvcef </h1> <strong>Copyright:</strong> <em> Group3 2024 </em>
         </p>
       </div>
     </footer>
